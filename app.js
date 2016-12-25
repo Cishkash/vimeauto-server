eb44c3b99b8bd24192172d4a1ceabd95a1f51543
Robby Mitchell <robjmitch@gmail.com>
2016-12-24 18:28:23 -0800

    Adds fetching of the access token

--- app.js
+++ app.js
@@ -4,18 +4,24 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var https = require('https');
+var env = require('./.env');
+
+// Create a base64 encoded string from the clientId and secret
+var encodedCredentials = new Buffer(env.clientId + ':' + env.clientSecret).toString('base64');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var categories = require('./routes/categories');
 
 var app = express();
+
+var bearerToken;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -24,6 +30,43 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/categories', categories);
+
+// Fetch app level bearerToken object
+bearerToken(bearerToken);
+
+function bearerToken(token) {
+  if (token) { return; }
+
+  var options = {
+    hostname: 'api.vimeo.com',
+    path: '/oauth/authorize/client?grant_type=client_credentials',
+    method: 'POST',
+    headers: {
+      'Authorization': 'basic ' + encodedCredentials
+    }
+  }
+
+  var req = https.request(options, (res) => {
+    res.on('data', (d) => {
+      process.stdout.write(d);
+    });
+  });
+
+  bearerToken = req;
+  req.end();
+}
+
+// CORS related stuff
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Methods", "GET, POST");
+  res.header("Authorization", 'Bearer ' + bearerToken.access_token);
+  console.log('Request url: ' + req.url);
+  next();
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
