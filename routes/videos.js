65247ccafce2b77d4607454222dfe4eba52b9f10
Robby Mitchell <robjmitch@gmail.com>
2017-01-15 14:55:47 -0800

    Adjustments to the video id and amount of results sent from the categories route

--- routes/videos.js
+++ routes/videos.js
@@ -5,6 +5,12 @@ var router = express.Router();
 
 
 router.get('/:video_id', function(req, res, next) {
+  /**
+   * Parses a usable date from the vimeo formatted date.
+   *
+   * @param  {String} date A date string consisting of a date and time.
+   * @return {Array}       An array of the split date and time.
+   */
   function parseDate(date) {
     return date.replace(/(?:T|\+)/g, ',').split(',');
   };
@@ -35,7 +41,7 @@ router.get('/:video_id', function(req, res, next) {
 
       dataObj.video.created_date = dateArray[0]
       dataObj.video.created_time = dateArray[1];
-      dataObj.video.id = 1;
+      dataObj.video.id = parseInt(dataObj.video.uri.split('/')[2]);
 
       res.send(dataObj);
     });
